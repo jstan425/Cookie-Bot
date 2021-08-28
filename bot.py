344af258f5c5d689398625e8b2c2dd706176810c
jstan425 <33200390+jstan425@users.noreply.github.com>
2021-08-28 16:37:49 +0800

    Initial Requirement commit

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord
+
+bot = discord.Client()
+
+@bot.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(bot))
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+bot.run('TOKEN')
