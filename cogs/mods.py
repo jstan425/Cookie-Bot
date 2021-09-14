c8b1b62384117e35626279b4e61c4ebf2b539c74
jstan425 <33200390+jstan425@users.noreply.github.com>
2021-09-14 21:02:39 +0800

    Updated mods.py

--- cogs/mods.py
+++ cogs/mods.py
@@ -1,4 +1,5 @@
 import disnake
+import asyncio
 from disnake.ext import commands
 
 
@@ -9,14 +10,23 @@ class Mods(commands.Cog):
     @commands.bot.slash_command(description="Kick user out of the Server.")
     async def kick(self, ctx, user : disnake.user,*,reason: None):
         await ctx.send(user.name + "have been kicked for" + reason)
-        await user.kick(reason = reason)
+        await disnake.guild.kick(reason = reason)
 
     @commands.bot.slash_command(description="Ban user from the Server.")
     async def ban(self, ctx, user : disnake.user,*,reason: None):
         await ctx.send(user.name + "have been ban for" + reason)
-        await user.ban(reason)
+        await disnake.guild.ban(reason)
     
     @commands.bot.slash_command(description="Unban user from the Server.")
     async def unban(self, ctx, user : disnake.user,*,reason: None):
         await ctx.send(user.name + "have been ban for" + reason)
-        await user.unban(reason)
+        await disnake.guild.unban(reason)
+    
+    #Tempban a user for certain durations and unban when it duration ends    
+    @commands.bot.slash_command(description="Temporary ban user for certain duration, and unban after duration ends.")
+    async def tempban(self, ctx, user : disnake.user, duration: int,* , reason: None):
+        await ctx.send(user.name + "have been temporary banned for" + reason + "for" + duration + "day.")
+        await disnake.guild.ban(user)
+        await asyncio.sleep(duration)
+        await disnake.guild.unban(user)
+        #Find a way to not only send message to the discord, but send DMs to the user as well.
