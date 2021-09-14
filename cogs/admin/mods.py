9e11d4c47ceccb7cf48ee1a43f59e25cd63b28b1
jstan425 <33200390+jstan425@users.noreply.github.com>
2021-09-14 18:16:43 +0800

    Relocate and rename admins cogs

---
+++ cogs/admin/mods.py
@@ -0,0 +1,21 @@
+import disnake
+from disnake.ext import commands
+
+class Mods(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.bot.slash_command(description="Kick user out of the Server.")
+    async def kick(self, ctx, user : disnake.user,*,reason: None):
+        await ctx.send(user.name + "have been kicked for" + reason)
+        await user.kick(reason = reason)
+
+    @commands.bot.slash_command(description="Ban user from the Server.")
+    async def ban(self, ctx, user : disnake.user,*,reason: None):
+        await ctx.send(user.name + "have been ban for" + reason)
+        await user.ban(reason)
+    
+    @commands.bot.slash_command(description="Unban user from the Server.")
+    async def unban(self, ctx, user : disnake.user,*,reason: None):
+        await ctx.send(user.name + "have been ban for" + reason)
+        await user.unban(reason)
