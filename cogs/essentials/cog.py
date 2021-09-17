8c564ae8d933f4e3878e63a1f0422ccd2eeeedd6
jstan425 <33200390+jstan425@users.noreply.github.com>
2021-09-17 22:11:47 +0800

    Created reload and ping command

---
+++ cogs/essentials/cog.py
@@ -0,0 +1,29 @@
+import disnake
+
+from disnake.ext import commands
+from inc.core import *
+
+
+class Essential(commands.Cog):
+    def __init__(self, bot:commands.Bot):
+        self.bot = bot
+
+    @commands.bot.slash_command(description="Ping the bot!")
+    async def ping(self, ctx):
+        await ctx.response.send_message(f"Cookie Crumps detected in {round(self.bot.latency * 1000)}ms")
+    
+    @commands.bot.slash_command(description="Reloads all cogs")
+    async def reload(self, ctx):
+        for folder in os.listdir("cogs"):
+            if os.path.exists(os.path.join("cogs", folder, "cog.py")):
+                self.bot.unload_extension(f"cogs.{folder}.cog")
+                self.bot.load_extension(f"cogs.{folder}.cog")
+        await ctx.response.send_message(f"Cogs fully reloaded.")
+        
+def setup(bot: commands.Bot):
+    bot.add_cog(Essential(bot))
+    print("Essentials cogs is now loaded")
+
+def teardown(bot: commands.Bot):
+    bot.remove_cog("Essential")
+    print("Essentials cog is now unloaded")
