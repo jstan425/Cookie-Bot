a000fbd91843b6471189c782f85d2eb06ce186af
jstan425 <33200390+jstan425@users.noreply.github.com>
2021-09-26 16:07:48 +0800

    Rewrite ban.py

--- cogs/admin/ban.py
+++ cogs/admin/ban.py
@@ -1,29 +1,65 @@
 import disnake, asyncio, datetime
 
+from disnake import embeds, Option, OptionType
 from disnake.ext import commands
 from datetime import datetime
 
+from disnake.ext.commands.core import command, has_permissions
+
 
 class Ban(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.bot.slash_command(description="Ban user from the Server.")
-    async def ban(self, ctx, user : disnake.user,*,reason: None):
-        await ctx.response.send_message(user.name + "have been ban for" + reason)
-        await disnake.guild.ban(reason)
-    
-    @commands.bot.slash_command(description="Unban user from the Server.")
-    async def unban(self, ctx, user : disnake.user):
-        await ctx.response.send_message(user.name + "have been unban!")
-        await disnake.guild.unban(user)
-    
-    #Tempban a user for certain durations and unban when it duration ends    
-    @commands.bot.slash_command(description="Temporary ban user for certain duration, and unban after duration ends.")
-    async def tempban(self, ctx, user : disnake.user, duration: int,* , reason: None):
-        await ctx.send(user.name + "have been temporary banned for" + reason + "for" + duration  + "day.")
-        await disnake.guild.ban(user)
-        await asyncio.sleep(duration)
-        await disnake.guild.unban(user)
-        #Find a way to not only send message to the discord, but send DMs to the user as well.
-        #Create a way to keep the time in a file and have the bot check for the time when the server is offline.
+
+@commands.bot.slash_command(
+    description="Ban user from the Server.",
+    options=[
+        Option("user", "Specify user would like to ban.", OptionType.user, True),
+        Option("reason", "Reason being punish", OptionType.string, False),
+    ],
+)
+@commands.has_permissions(ban_members=True)
+async def ban(ctx, user: disnake.user, *, reason: None):
+    embed = disnake.Embed(
+        description=f"✅ **{user.display_name}#{user.discriminator} is banned for {reason}",
+        color=disnake.Color.red(),
+    )
+    await ctx.channel.send(embed=embed)
+    await user.ban(reason)
+
+
+@commands.bot.slash_command(
+    description="Unban user from the Server.",
+    options=[Option("user", "Specify user would like to ban.", OptionType.user, True)],
+)
+@commands.has_permissions(ban_members=True)
+async def unban(ctx, user: disnake.user):
+    embed = disnake.Embed(
+        description=f"✅ **{user.display_name}#{user.discriminator} had been unbanned",
+        color=disnake.Color.green(),
+    )
+    await ctx.channel.send(embed=embed)
+    await user.unban(user)
+
+
+@commands.bot.slash_command(
+    description="Tempban specified user.",
+    options=[
+        Option("user", "Specify user would like to ban.", OptionType.user, True),
+        Option("time", "Specify duration.", OptionType.string, True),
+        Option("reason", "Reason being punish", OptionType.string, False),
+    ],
+)
+@commands.has_permissions(ban_members=True)
+async def tempban(ctx, user: disnake.member = None, time=str, *, reason=None):
+    time_convert = {"s": 1, "m": 60, "h": 3600, "d": 86400}
+    ban_time = int(time[0]) * time_convert[time[-1]]
+    embed = disnake.Embed(
+        description=f"✅ **{user.display_name}#{user.discriminator} had been tempban for {time} successfully**",
+        color=disnake.Color.green(),
+    )
+    await ctx.channel.send(embed=embed)
+    await user.ban(reason)
+    await asyncio.sleep(ban_time)
+    await user.unban(user)
