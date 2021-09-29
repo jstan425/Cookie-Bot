8f835fa6b0e6ae66ff9902e7e187fb78d42231b1
Decap <decap@opentorn.com>
2021-09-29 13:55:19 +0100

    sqlite and tempban

--- cogs/admin/ban.py
+++ cogs/admin/ban.py
@@ -1,64 +1,93 @@
 import disnake, asyncio, datetime
 
-from disnake.ext import commands
+from disnake.ext import commands, tasks
 from disnake.ext.commands import Param
-from datetime import datetime
+from datetime import datetime, timedelta
+from util.db import *
 
 
 class Ban(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.temp_ban_checker.start()
 
+    @commands.slash_command(description="Ban user from the Server.")
+    async def ban(
+        self,
+        inter: disnake.ApplicationCommandInteraction,
+        user: disnake.User = Param(None, desc="Specify a user"),
+        reason=Param(None, desc="Provide a reason"),
+    ):
+        embed = disnake.Embed(
+            description=(
+                f"✅ **{user.display_name}#{user.discriminator} is banned for {reason}"
+            ),
+            color=disnake.Color.red(),
+        )
+        await inter.channel.send(embed=embed)
+        await user.ban(user, reason=reason)
 
-@commands.slash_command(description="Ban user from the Server.")
-async def ban(
-    self,
-    inter: disnake.ApplicationCommandInteraction,
-    user: disnake.User = Param(None, desc="Specify a user"),
-    reason=Param(None, desc="Provide a reason"),
-):
-    embed = disnake.Embed(
-        description=(
-            f"✅ **{user.display_name}#{user.discriminator} is banned for {reason}"
-        ),
-        color=disnake.Color.red(),
-    )
-    await inter.channel.send(embed=embed)
-    await user.ban(user, reason=reason)
+    @commands.slash_command(description="Unban user from the Server.")
+    async def unban(
+        self,
+        inter: disnake.ApplicationCommandInteraction,
+        user: disnake.User = Param(None, desc="Specify a user"),
+        reason: str = Param(None, desc="Provide a reason"),
+    ):
+        embed = disnake.Embed(
+            description=(
+                f"✅ **{user.display_name}#{user.discriminator} had been unbanned"
+            ),
+            color=disnake.Color.green(),
+        )
+        await inter.channel.send(embed=embed)
+        await user.unban()
 
+    @commands.slash_command(description="Tempban user from the server.")
+    async def tempban(
+        self,
+        inter: disnake.ApplicationCommandInteraction,
+        user: disnake.User = Param(None, desc="Specify a user"),
+        time: str = Param(None, desc="Specify a time"),
+        reason: str = Param(None, desc="Provide a reason"),
+    ):
+        time_convert = {"s": 1, "m": 60, "h": 3600, "d": 86400}
+        ban_time = int(time[0]) * time_convert[time[-1]]
+        end_time = datetime.now() + timedelta(seconds=ban_time)
+        embed = disnake.Embed(
+            description=(
+                f"✅ **{user.display_name}#{user.discriminator} had been tempban for {time} successfully**"
+            ),
+            color=disnake.Color.green(),
+        )
 
-@commands.slash_command(description="Unban user from the Server.")
-async def unban(
-    self,
-    inter: disnake.ApplicationCommandInteraction,
-    user: disnake.User = Param(None, desc="Specify a user"),
-    reason: str = Param(None, desc="Provide a reason"),
-):
-    embed = disnake.Embed(
-        description=(f"✅ **{user.display_name}#{user.discriminator} had been unbanned"),
-        color=disnake.Color.green(),
-    )
-    await inter.channel.send(embed=embed)
-    await user.unban(user)
+        conn = create_connection(r"sqlite.db")
+        sql = f" INSERT INTO temp_ban(guild_id,user_id,end_time) VALUES({inter.guild.id},{user.id},{end_time.timestamp()}) "
+        cur = conn.cursor()
+        cur.execute(sql)
+        conn.commit()
+        await user.ban(reason=reason)
+        await inter.response.send_message(embed=embed)
+        # await asyncio.sleep(ban_time)
+        # await user.unban(user)
 
+    # TODO change this to how often you want this to run, i'd suggest every minute
+    @tasks.loop(minutes=1.0)
+    async def temp_ban_checker(self):
+        conn = create_connection(r"sqlite.db")
+        timestamp_now = datetime.now().timestamp()
+        cur = conn.cursor()
+        cur.execute(f"SELECT * FROM temp_ban WHERE end_time < {timestamp_now}")
+        rows = cur.fetchall()
+        for row in rows:
+            selected_guild = self.bot.get_guild(row[1])
+            bans = await selected_guild.bans()
+            for ban in bans:
+                if row[2] == ban.user.id:
+                    print (f"unbanning {ban.user}")
+                    await selected_guild.unban(ban.user)
 
-@commands.slash_command(description="Tempban user from the server.")
-async def tempban(
-    self,
-    inter: disnake.ApplicationCommandInteraction,
-    user: disnake.User = Param(None, desc="Specify a user"),
-    time: str = Param(None, desc="Specify a time"),
-    reason: str = Param(None, desc="Provide a reason"),
-):
-    time_convert = {"s": 1, "m": 60, "h": 3600, "d": 86400}
-    ban_time = int(time[0]) * time_convert[time[-1]]
-    embed = disnake.Embed(
-        description=(
-            f"✅ **{user.display_name}#{user.discriminator} had been tempban for {time} successfully**"
-        ),
-        color=disnake.Color.green(),
-    )
-    await user.ban(user, reason=reason)
-    await inter.channel.send(embed=embed)
-    await asyncio.sleep(ban_time)
-    await user.unban(user)
+    @temp_ban_checker.before_loop
+    async def before_printer(self):
+        print("waiting...")
+        await self.bot.wait_until_ready()
--- cogs/admin/mute.py
+++ cogs/admin/mute.py
@@ -10,25 +10,24 @@ class Mute(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
-# Timed mute this format: 1d, 20s, 30m, etc..
-@commands.slash_command(description="Mute specified user.")
-async def tempmute(
-    self,
-    inter: disnake.ApplicationCommandInteraction,
-    user: disnake.User = Param(None, desc="Specify a user"),
-    time: str = Param(None, desc="Specify a time"),
-    reason: str = Param(None, desc="Provide a reason"),
-):
-    role = disnake.utils.get(inter.guild.roles, name="Muted")
-    time_convert = {"s": 1, "m": 60, "h": 3600, "d": 86400}
-    mute_time = int(time[0]) * time_convert[time[-1]]
-    await user.add_roles(role)
-    embed = disnake.Embed(
-        description=f"✅ **{user.display_name}#{user.discriminator} muted successfuly**",
-        color=disnake.Color.green(),
-    )
-    # TODO: change send embed method.
-    await inter.channel.send(embed=embed)
-    await asyncio.sleep(mute_time)
-    await user.remove_roles(role)
+    # Timed mute this format: 1d, 20s, 30m, etc..
+    @commands.slash_command(description="Mute specified user.")
+    async def tempmute(
+        self,
+        inter: disnake.ApplicationCommandInteraction,
+        user: disnake.User = Param(None, desc="Specify a user"),
+        time: str = Param(None, desc="Specify a time"),
+        reason: str = Param(None, desc="Provide a reason"),
+    ):
+        role = disnake.utils.get(inter.guild.roles, name="Muted")
+        time_convert = {"s": 1, "m": 60, "h": 3600, "d": 86400}
+        mute_time = int(time[0]) * time_convert[time[-1]]
+        await user.add_roles(role)
+        embed = disnake.Embed(
+            description=f"✅ **{user.display_name}#{user.discriminator} muted successfuly**",
+            color=disnake.Color.green(),
+        )
+        # TODO: change send embed method.
+        await inter.channel.send(embed=embed)
+        await asyncio.sleep(mute_time)
+        await user.remove_roles(role)
