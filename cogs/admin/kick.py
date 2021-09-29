34b926ddc848265f9fb44797fba06968fd6c89a7
Decap <decap@opentorn.com>
2021-09-29 11:09:34 +0100

    code fix

--- cogs/admin/kick.py
+++ cogs/admin/kick.py
@@ -1,17 +1,18 @@
-import disnake 
+import disnake
 from disnake.ext import commands
-from disnake.ext.commands import Param 
+from disnake.ext.commands import Param
+
 
 class Kick(commands.Cog):
-    def __init__(self, bot:commands.Bot):
-        self.bot=bot
-        
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
     @commands.slash_command(description="Unban user from the Server.")
-    
-    async def kick(self, 
-              inter: disnake.ApplicationCommandInteraction,
-              user: disnake.user = Param(None, desc="Specify a user"), 
-              reason: str = Param(None, desc="Provide a reason"),
-):
+    async def kick(
+        self,
+        inter: disnake.ApplicationCommandInteraction,
+        user: disnake.User = Param(None, desc="Specify a user"),
+        reason: str = Param(None, desc="Provide a reason"),
+    ):
         await inter.send(user.name + "have been kicked for" + reason)
-        await disnake.guild.kick(reason = reason)
+        await disnake.guild.kick(reason=reason)
--- cogs/admin/mute.py
+++ cogs/admin/mute.py
@@ -13,21 +13,22 @@ class Mute(commands.Cog):
 
 # Timed mute this format: 1d, 20s, 30m, etc..
 @commands.slash_command(description="Mute specified user.")
-async def tempmute(self, 
-              inter: disnake.ApplicationCommandInteraction,
-              user: disnake.user = Param(None, desc="Specify a user"),
-              time: str = Param(None, desc="Specify a time"), 
-              reason: str = Param(None, desc="Provide a reason"),
+async def tempmute(
+    self,
+    inter: disnake.ApplicationCommandInteraction,
+    user: disnake.User = Param(None, desc="Specify a user"),
+    time: str = Param(None, desc="Specify a time"),
+    reason: str = Param(None, desc="Provide a reason"),
 ):
-            role = disnake.utils.get(inter.guild.roles, name="Muted")
-            time_convert = {"s": 1, "m": 60, "h": 3600, "d": 86400}
-            mute_time = int(time[0]) * time_convert[time[-1]]
-            await user.add_roles(role)
-            embed = disnake.Embed(
-                description=f"✅ **{user.display_name}#{user.discriminator} muted successfuly**",
-                color=disnake.Color.green(),
+    role = disnake.utils.get(inter.guild.roles, name="Muted")
+    time_convert = {"s": 1, "m": 60, "h": 3600, "d": 86400}
+    mute_time = int(time[0]) * time_convert[time[-1]]
+    await user.add_roles(role)
+    embed = disnake.Embed(
+        description=f"✅ **{user.display_name}#{user.discriminator} muted successfuly**",
+        color=disnake.Color.green(),
     )
-            # TODO: change send embed method.
-            await inter.channel.send(embed=embed)
-            await asyncio.sleep(mute_time)
-            await user.remove_roles(role)
+    # TODO: change send embed method.
+    await inter.channel.send(embed=embed)
+    await asyncio.sleep(mute_time)
+    await user.remove_roles(role)
