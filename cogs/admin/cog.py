34fb8bb37b97ad760e69fd210471a75d9179d915
jstan425 <33200390+jstan425@users.noreply.github.com>
2021-10-12 09:23:29 +0800

    Cleanup

--- cogs/admin/cog.py
+++ cogs/admin/cog.py
@@ -1,17 +1,19 @@
 import logging
 
 from disnake.ext import commands
+
 from .ban import Ban
 from .kick import Kick
 from .mute import Mute
+from .purge import Purge
 
 
-    
 def setup(bot: commands.Bot):
     bot.add_cog(Ban(bot))
     bot.add_cog(Kick(bot))
     bot.add_cog(Mute(bot))
-    print('Admin cog is now loaded.' + "\n")
+    bot.add_cog(Purge(bot))
+    print("Admin cog is now loaded." + "\n")
     logger = logging.getLogger("disnake")
     logger.info("Added Admin Cog.")
 
@@ -20,6 +22,7 @@ def teardown(bot: commands.Bot):
     bot.remove_cog("Ban")
     bot.remove_cog("Kick")
     bot.remove_cog("Mute")
-    print('Admin cog is now unloaded.' + "\n")
+    bot.remove_cog("Purge")
+    print("Admin cog is now unloaded." + "\n")
     logger = logging.getLogger("disnake")
     logger.info("Removed Admin Cog.")
