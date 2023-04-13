63583aea08010b81099e97fa56a4c81f4dc4ed7c
Joe Tan <tan.jorshun@gmail.com>
2023-04-13 13:48:10 +0000

    refactor: kick

--- cogs/admin/kick.py
+++ cogs/admin/kick.py
@@ -7,12 +7,22 @@ class Kick(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.slash_command(description="Unban user from the Server.")
+    @commands.slash_command(description = "Kick user from the Server.")
     async def kick(
         self,
         inter: disnake.ApplicationCommandInteraction,
-        user: disnake.User = Param(None, desc="Specify a user"),
-        reason: str = Param(None, desc="Provide a reason"),
+        user: disnake.User = Param(None, desc = "Specify a user"),
+        reason: str = Param(None, desc = "Provide a reason"),
     ):
-        await inter.send(user.name + "have been kicked for" + reason)
-        await disnake.guild.kick(reason=reason)
+        if user is None:
+            await inter.channel.send("Specify a user to kick.")
+            return
+        embed = disnake.Embed(
+            description = (
+                f"✅ **{user.display_name}#{user.discriminator} is kicked!"
+                ),
+                color = disnake.Color.red(),
+        )   
+        await inter.send(user.name + "have been kicked!"),
+        await disnake.guild.kick(reason = reason)
+
