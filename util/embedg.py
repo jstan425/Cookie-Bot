10958b8086ccb803bfaa3288be46efd8a2e6e4a7
Joe Tan <tan.jorshun@gmail.com>
2023-04-13 11:05:02 +0000

    feat: general embed generator

--- util/embedg.py
+++ util/embedg.py
@@ -1,6 +1,7 @@
 import os
 import disnake
 import datetime
+import asyncio
 
 from disnake.ext import commands
 from disnake.ext.commands.bot import Bot
@@ -31,3 +32,20 @@ class banEmbed(disnake.embed):
         local_desc = f'Reason: {reason}'
         super().__init__(color=NEGATIVECOLOR, title=local_title, description=local_desc,)
         self.set_footer(text=return_current_time())
+
+class embed(disnake.embed):
+    async def embed_new(title = None, description = None, color = disnake.Embed.Empty, user : disnake.Member = None, fields : dict = None, thumbnail = None):
+
+        embed = disnake.Embed(title=title, description=description, color=color)
+
+        if fields != None:
+            for k in fields:
+                embed.add_field(name=k, value=fields[k], inline=False)
+
+        if user != None:
+            embed.set_thumbnail(url=user.display_avatar.url)
+
+        if thumbnail != None:
+            embed.set_thumbnail(url=thumbnail)
+        
+        return embed
