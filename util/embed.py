3b6877d80958ab212d2412278b75f4ea8b509187
Joe Tan <tan.jorshun@gmail.com>
2023-04-13 13:48:35 +0000

    add: kickEmbed

--- util/embed.py
+++ util/embed.py
@@ -10,35 +10,48 @@ from dotenv import load_dotenv
 
 def return_current_time():
     time = datetime.datetime.utcnow()
-    return time.strftime('%A, %b %d %H:%M')
+    return time.strftime("%A, %b %d %H:%M")
 
 
 class embed(commands.cog):
-    def __init__(self, bot:commands.bot):
-        self.bot=Bot
+    def __init__(self, bot: commands.bot):
+        self.bot = Bot
+
 
 NEGATIVECOLOR = 0xD00000
 SLIGHTLYNEGATIVECOLOR = 0xF26E00
 POSITIVECOLOR = 0x419400
-NEUTRALCOLOR = 0x607d8b
+NEUTRALCOLOR = 0x607D8B
 DELETEDCOLOR = 0x3399CC
 EDITEDCOLOR = 0x00E4C9
-USEREDITCOLOR = 0xe9e6c2
+USEREDITCOLOR = 0xE9E6C2
 ROLECOLOR = 0x800080
+
 
 class banEmbed(disnake.embed):
     def __init__(self, banned_user: disnake.user, resp_mod: disnake.user, reason: str):
-        local_title = f'{banned_user.name} was banned by {resp_mod.name}'
-        local_desc = f'Reason: {reason}'
-        super().__init__(color=NEGATIVECOLOR, title=local_title, description=local_desc,)
+        local_title = f"{banned_user.name} was banned by {resp_mod.name}"
+        local_desc = f"Reason: {reason}"
+        super().__init__(
+            color=NEGATIVECOLOR,
+            title=local_title,
+            description=local_desc,
+        )
         self.set_footer(text=return_current_time())
 
+
 class kickEmbed(disnake.embed):
-        def __init__(self, kicked_user: disnake.user, resp_mod: disnake.user, reason: str):
-                local_title = f'{kicked_user.name} was banned by {resp_mod.name}'
-                local_desc = f'Reason: {reason}'
-                super().__init__(color=NEGATIVECOLOR, title=local_title, description=local_desc,)
-                self.set_footer(text=return_current_time())
+    def __init__(self, kicked_user: disnake.user, resp_mod: disnake.user, reason: str):
+        local_title = f"{kicked_user.name} was banned by {resp_mod.name}"
+        local_desc = f"Reason: {reason}"
+        super().__init__(
+            color=NEGATIVECOLOR,
+            title=local_title,
+            description=local_desc,
+        )
+        self.set_footer(text=return_current_time())
+
+
 # class embed(disnake.embed):
 #     async def embed_new(title = None, description = None, color = disnake.Embed.Empty, user : disnake.Member = None, fields : dict = None, thumbnail = None):
 
@@ -52,5 +65,5 @@ class kickEmbed(disnake.embed):
 #             embed.set_thumbnail(url=user.display_avatar.url)
 
 #         if thumbnail != None:
-#             embed.set_thumbnail(url=thumbnail) 
+#             embed.set_thumbnail(url=thumbnail)
 #         return embed
