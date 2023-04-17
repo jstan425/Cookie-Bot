d87856ae23a590c372db70c2b15eb5bf7faca57c
Joe Tan <tan.jorshun@gmail.com>
2023-04-17 14:13:01 +0000

    chore: rename folders

--- utils/cog.py
+++ utils/cog.py
@@ -10,7 +10,7 @@ def setup(bot: commands.Bot):
     bot.add_cog(banEmbed(bot))
     bot.add_cog(kickEmbed(bot))
 
-    print('Util cog is now loaded.' + "\n")
+    print('Utils cog is now loaded.' + "\n")
     logger = logging.getLogger("disnake")
     logger.info("Added Util Cog.")
 
@@ -20,6 +20,6 @@ def teardown(bot: commands.Bot):
     bot.remove_cog("banEmbed")
     bot.remove_cog("kickEmbed")
 
-    print('Util cog is now unloaded.' + "\n")
+    print('Utils cog is now unloaded.' + "\n")
     logger = logging.getLogger("disnake")
     logger.info("Removed Util Cog.")
